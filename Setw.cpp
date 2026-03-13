beaaf893201c7cf76408eda2633dc4528a12bd3a
shiva mahavar <sbmahavar@gmail.com>
2026-03-13 18:22:51 +0530

    Add Setw.cpp to print integer values

---
+++ Setw.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 123;
+    int b = 12345;
+    int c = 1234;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+
+    return 0;
+}
