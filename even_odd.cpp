371818cbaa89adb4ac739ffc37437d4661aa6ab9
shiva mahavar <sbmahavar@gmail.com>
2026-03-13 18:15:26 +0530

    Add even-odd check in even_odd.cpp

---
+++ even_odd.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+int n;
+
+cout << "enter any number: ";
+
+cin >> n;
+
+if (n % 2 == 0)
+
+cout << n << " is even"; // Corrected this line
+
+else
+
+cout << n << " is odd"; // Corrected this line
+
+return 0;
+
+}
