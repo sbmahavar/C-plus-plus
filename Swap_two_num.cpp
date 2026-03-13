3e2d53c8d7ca02a75ff05b2541f6dc73840ce442
shiva mahavar <sbmahavar@gmail.com>
2026-03-13 18:21:33 +0530

    Implement swap of two numbers in C++
    Write a program to swap 2 number using temp variable

---
+++ Swap_two_num.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a = 6, b = 10, temp;
+
+    
+    cout << "Before Swapping: " << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    
+    temp = a;   
+    a = b;       
+    b = temp;    
+
+     
+    cout << "\nAfter Swapping: " << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
